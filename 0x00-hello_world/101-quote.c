d6280734e9a8235cf2bd3d204caa32cfd04ef3ab
omvriio <unimarghadi@gmail.com>
2023-08-16 13:58:12 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 /**
- *  * main - main function
- *   *
- *    * Return: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * 
+ * Return: Always 1.
  */
 int main(void)
 {
-	char *string;
-	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korpar, 2015-10-2019";
-
-	string = malloc(strlen(str1) + strlen(str2) + 1);
-	strcpy(string, str1);
-	strcpy(string, str2);
-	write(2, string, sizeof(string));
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
 }
